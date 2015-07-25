64e953e3cc4c52b470537e5855826507b67e6ea6
Llorenç Muntaner <llorenc.muntaner@gmail.com>
2015-07-25 17:00:01 +0200

    dashboard model

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -7,9 +7,27 @@ class DashboardsController < ApplicationController
   end
 
   def create
-    var user = User.find(params[:user_id])
-    user.
-    var answers = params[:answers]
-    IntegrationRetriever.create_dashboard_integrations(answers)
+    user = User.find(params[:user_id])
+    dashboard = user.dashboard.new
+    dashboard.save
+    answers = params[:answers]
+    IntegrationRetriever.create_dashboard_integrations(dashboard, answers)
+    data = File.read("#{Rails.root}/app/services/dashboard_form.json")
+    tempHash = JSON.parse(data)
+    personalize_webhook = "https://kids-board.herokuapp.com/users/#{@user.id}/setup_forms"
+    tempHash["webhook_submit_url"] = personalize_webhook
+
+    generateTF = Faraday.new(:url => 'https://api.typeform.io') do |faraday|
+      faraday.request  :url_encoded             # form-encode POST params
+      faraday.response :logger                  # log requests to STDOUT
+      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+    end
+
+    response = generateTF.post do |req|
+      req.url '/v0.3/forms'
+      req.headers["X-API-TOKEN"] = '487dd5b20dbd7f641fd79edbb77915ef'
+      req.body = tempHash.to_json
+    end
+
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
   has_many :setup_forms
+  has_many :dashboards
 end
